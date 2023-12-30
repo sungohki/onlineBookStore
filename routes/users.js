40aed9c22b9c3c94cec0a9e38b294de1ebd98a0a
sungohki <sokim1201@naver.com>
2023-12-31 02:39:53 +0900

    new : create users.js for router of user page

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(express.json());
+
+// 회원 가입
+router.post('/join', (req, res) => {});
+
+// 로그인
+router.post('/login', (req, res) => {});
+
+// 비밀번호 초기화 요청
+router.post('/reset', (req, res) => {});
+
+// 비밀번호 초기화
+router.put('/reset', (req, res) => {});
+
+export default router;
