cbcc8d931e01f2a737e4a015f2d9e1c9279c488e
sungohki <sokim1201@naver.com>
2024-01-02 14:18:13 +0900

    modified : add login controller

--- controller/UserControllder.js
+++ controller/UserControllder.js
@@ -1,5 +1,9 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const join = (req, res) => {
   const { email, password } = req.body;
@@ -11,8 +15,49 @@ const join = (req, res) => {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
     }
-    res.status(StatusCodes.CREATED).json(results);
+    return res.status(StatusCodes.CREATED).json(results);
   });
 };
 
-module.exports = join;
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = 'SELECT * FROM users WHERE email = ?';
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+    }
+    const loginUser = results[0];
+    if (loginUser && loginUser.password == password) {
+      const token = jwt.sign(
+        {
+          email: loginUser.email,
+        },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: '5m',
+          issuer: 'sungohki',
+        }
+      );
+      res.cookie('token', token, {
+        httpOnly: true,
+      });
+      console.log(token);
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  });
+};
+
+const passwordResetRequest = (req, res) => {};
+
+const passwordReset = (req, res) => {};
+
+module.exports = {
+  join,
+  login,
+  passwordResetRequest,
+  passwordReset,
+};
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 // const conn = require('../mariadb.js');
 // const { StatusCodes } = require('http-status-codes');
-
-const join = require('../controller/UserControllder.js');
+const {
+  join,
+  login,
+  passwordResetRequest,
+  passwordReset,
+} = require('../controller/UserControllder.js');
 
 router.use(express.json());
 
@@ -11,18 +15,12 @@ router.use(express.json());
 router.post('/join', join);
 
 // 로그인
-router.post('/login', (req, res) => {
-  res.json({ message: 'login' });
-});
+router.post('/login', login);
 
 // 비밀번호 초기화 요청
-router.post('/reset', (req, res) => {
-  res.json({ message: 'request refresh password' });
-});
+router.post('/reset', passwordResetRequest);
 
 // 비밀번호 초기화
-router.put('/reset', (req, res) => {
-  res.json({ message: 'refresh password' });
-});
+router.put('/reset', passwordReset);
 
 module.exports = router;
