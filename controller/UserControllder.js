afbdea9a7e86924a7bc77679db531c17069167b6
sungohki <sokim1201@naver.com>
2024-01-03 14:23:49 +0900

    modified : Updated password encryption syntax

--- controller/UserControllder.js
+++ controller/UserControllder.js
@@ -2,13 +2,20 @@ const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const crypto = require('crypto');
 
 dotenv.config();
 
 const join = (req, res) => {
   const { email, password } = req.body;
-  let sql = 'INSERT INTO users (email, password) VALUES (?, ?)';
-  let values = [email, password];
+
+  const salt = crypto.randomBytes(10).toString('base64');
+  const hashPassword = crypto
+    .pbkdf2Sync(password, salt, 10000, 10, 'sha512')
+    .toString('base64');
+
+  let sql = 'INSERT INTO users (email, password, salt) VALUES (?, ?, ?)';
+  let values = [email, hashPassword, salt];
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -29,7 +36,10 @@ const login = (req, res) => {
       return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
     }
     const loginUser = results[0];
-    if (loginUser && loginUser.password == password) {
+    const hashPassword = crypto
+      .pbkdf2Sync(password, loginUser.salt, 10000, 10, 'sha512')
+      .toString('base64');
+    if (loginUser && loginUser.password == hashPassword) {
       const token = jwt.sign(
         {
           email: loginUser.email,
@@ -51,9 +61,53 @@ const login = (req, res) => {
   });
 };
 
-const passwordResetRequest = (req, res) => {};
+const passwordResetRequest = (req, res) => {
+  const { email } = req.body;
 
-const passwordReset = (req, res) => {};
+  let sql = 'SELECT * FROM users  WHERE email = ?';
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+    }
+
+    const user = results[0];
+    if (user) {
+      return res.status(StatusCodes.OK).json({
+        email: email,
+      });
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  });
+};
+
+const passwordReset = (req, res) => {
+  const { email, password } = req.body;
+
+  const salt = crypto.randomBytes(10).toString('base64');
+  const hashPassword = crypto
+    .pbkdf2Sync(password, salt, 10000, 10, 'sha512')
+    .toString('base64');
+
+  let sql = `UPDATE users 
+        SET password = ?, salt = ? 
+        WHERE email = ?`;
+  let values = [hashPassword, salt, email];
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+    }
+
+    if (results.affectedRows) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+    }
+  });
+};
 
 module.exports = {
   join,
