6ea4dc05f9ab7654cc76e059af8b503cfce20868
sungohki <sokim1201@naver.com>
2024-01-03 16:35:53 +0900

    modified : Integrate search function by category with overall search

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,13 +1,46 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
-// 전체 도서 조회
-const allBooks = (req, res) => {};
+// 전체 도서 | 카테고리별 도서 조회
+const allBooks = (req, res) => {
+  const { category_id } = req.query;
+
+  if (category_id) {
+    const sql = 'SELECT * FROM books WHERE category_id = ?';
+    conn.query(sql, category_id, (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      if (results.length) return res.status(StatusCodes.OK).json(results);
+      else return res.status(StatusCodes.NOT_FOUND).end();
+    });
+  } else {
+    const sql = 'SELECT * FROM books';
+    conn.query(sql, (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
+};
 
 // 개별 도서 조회
-const bookDetail = (req, res) => {};
+const bookDetail = (req, res) => {
+  const { id } = req.params;
+  const bookId = parseInt(id);
 
-// 카테고리별 도서 목록 조회
-const booksByCategory = (req, res) => {};
+  const sql = 'SELECT * FROM books WHERE id = ?';
+  conn.query(sql, bookId, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results[0]) return res.status(StatusCodes.OK).json(results[0]);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
+};
 
-module.exports = { allBooks, bookDetail, booksByCategory };
+module.exports = { allBooks, bookDetail };
--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {
-  allBooks,
-  bookDetail,
-  booksByCategory,
-} = require('../controller/BookController');
+const { allBooks, bookDetail } = require('../controller/BookController.js');
 
 router.use(express.json());
 
-// 전체 도서 조회
+// 전체 도서  && 카테고리별 도서 조회
 router.get('/', allBooks);
 // 개별 도서 조회
 router.get('/:id', bookDetail);
-// 카테고리 별 도서 목록 조회
-router.get('/', booksByCategory);
 
 module.exports = router;
