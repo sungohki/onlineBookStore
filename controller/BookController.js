934e12c1f900cfbd66dc3ca26fc82a68df65cf58
sungohki <sokim1201@naver.com>
2024-01-04 16:16:33 +0900

    modified : Addition of book-related API code (improvement)

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,28 +3,30 @@ const { StatusCodes } = require('http-status-codes');
 
 // 전체 도서 | 카테고리별 도서 조회
 const allBooks = (req, res) => {
-  const { category_id } = req.query;
+  const { category_id, news } = req.query;
+  let sql = 'SELECT * FROM books';
+  let values = [];
 
-  if (category_id) {
-    const sql = 'SELECT * FROM books WHERE category_id = ?';
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      if (results.length) return res.status(StatusCodes.OK).json(results);
-      else return res.status(StatusCodes.NOT_FOUND).end();
-    });
-  } else {
-    const sql = 'SELECT * FROM books';
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
+  if (category_id && news) {
+    sql += ` WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND now()`;
+    values = [category_id, news];
+  } else if (category_id) {
+    sql += ` WHERE category_id = ?`;
+    values = category_id;
+  } else if (news) {
+    sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND now()`;
+    values = news;
   }
+  sql += ';';
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.length) return res.status(StatusCodes.OK).json(results);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
 };
 
 // 개별 도서 조회
@@ -32,7 +34,9 @@ const bookDetail = (req, res) => {
   const { id } = req.params;
   const bookId = parseInt(id);
 
-  const sql = 'SELECT * FROM books WHERE id = ?';
+  // const sql = 'SELECT * FROM books WHERE id = ?';
+  const sql = `SELECT * FROM books LEFT JOIN 
+  category ON books.category_id = category.id WHERE books.id = ?`;
   conn.query(sql, bookId, (err, results) => {
     if (err) {
       console.log(err);
