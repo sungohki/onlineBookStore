9be3a259e9591816a22eeab10827e5e6a233868f
sungohki <sokim1201@naver.com>
2024-01-05 17:27:19 +0900

    modified : add paging func

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,21 +3,21 @@ const { StatusCodes } = require('http-status-codes');
 
 // 전체 도서 | 카테고리별 도서 조회
 const allBooks = (req, res) => {
-  const { category_id, news } = req.query;
+  const { category_id, news, limit, currentPage } = req.query;
+  const offset = parseInt(limit) * (parseInt(currentPage) - 1);
   let sql = 'SELECT * FROM books';
-  let values = [];
+  let values = [parseInt(limit), offset];
 
   if (category_id && news) {
-    sql += ` WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND now()`;
-    values = [category_id, news];
+    sql += ` WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+    values = [category_id, ...values];
   } else if (category_id) {
     sql += ` WHERE category_id = ?`;
-    values = category_id;
+    values = [category_id, ...values];
   } else if (news) {
-    sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND now()`;
-    values = news;
+    sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
   }
-  sql += ';';
+  sql += ` LIMIT ? OFFSET ?`;
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -35,7 +35,7 @@ const bookDetail = (req, res) => {
   const bookId = parseInt(id);
 
   // const sql = 'SELECT * FROM books WHERE id = ?';
-  const sql = `SELECT * FROM books LEFT JOIN 
+  const sql = `SELECT * FROM books LEFT JOIN
   category ON books.category_id = category.id WHERE books.id = ?`;
   conn.query(sql, bookId, (err, results) => {
     if (err) {
