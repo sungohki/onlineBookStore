81a79bcea3c0fc86a6e8b1113d55c8bb6140f82a
sungohki <sokim1201@naver.com>
2024-01-08 13:34:07 +0900

    modified : modify routing

--- routes/carts.js
+++ routes/carts.js
@@ -11,12 +11,9 @@ const {
 // 장바구니 담기
 router.post('/', addToCart);
 // 장바구니 아이템 목록 조회
+// + (장바구니에서 선택한) 주문 예상 상품 목록 조회
 router.get('/', getCartItems);
 // 장바구니 도서 삭제
 router.delete('/:id', removeCartItem);
 
-// // 장바구니에서 선택한 주문 예상 상품 목록 조회
-// router.get('/carts', (res, req) => {
-//   res.json(' 장바구니에서 선택한 주문 예상 상품 목록 조회');
-// });
 module.exports = router;
