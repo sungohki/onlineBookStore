b972d5b3b80b59ea6dc2f530c985b5343b5eff92
sungohki <sokim1201@naver.com>
2024-01-08 13:33:47 +0900

    modified : be advanced functions

--- controller/CartController.js
+++ controller/CartController.js
@@ -18,14 +18,17 @@ const addToCart = (req, res) => {
   });
 };
 // 장바구니 아이템 목록 조회
+// + (장바구니에서 선택한) 주문 예상 상품 목록 조회
 const getCartItems = (req, res) => {
-  const { user_id } = req.body;
+  const { user_id, selected } = req.body;
+
   let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price
     FROM cartItems LEFT
     JOIN books ON cartItems.book_id = books.id
-    WHERE user_id = ?;`;
+    WHERE user_id = ? AND cartItems.id IN (?);`;
+  const values = [user_id, selected];
 
-  conn.query(sql, user_id, (err, results) => {
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       res.status(StatusCodes.BAD_REQUEST).end();
@@ -35,6 +38,11 @@ const getCartItems = (req, res) => {
 };
 // 장바구니 아이템 삭제
 const removeCartItem = (req, res) => {
+  const { id } = req.params;
+  const { user_id } = req.body;
+
+  const sql = `DELETE FROM cartItems WHERE id = ?`;
+  const values = [id, user_id];
   conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
