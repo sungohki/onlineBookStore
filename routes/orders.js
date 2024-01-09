624642c89ac92a753313072a8dd31b1feb98d39d
sungohki <sokim1201@naver.com>
2024-01-09 14:23:36 +0900

    feat: Specify callback function

--- routes/orders.js
+++ routes/orders.js
@@ -3,17 +3,17 @@ const router = express.Router();
 
 router.use(express.json());
 
+const {
+  order,
+  getOrders,
+  getOrderDetail,
+} = require('../controller/OrderController');
+
 // 주문 하기
-router.post('/', (res, req) => {
-  res.json('주문 하기');
-});
+router.post('/', order);
 // 주문 목록 조회
-router.get('/', (res, req) => {
-  res.json('주문 목록 조회');
-});
+router.get('/', getOrders);
 // 주문 상세 상품 조회
-router.get('/:id', (res, req) => {
-  res.json('주문 상세 상품 조회');
-});
+router.get('/:id', getOrderDetail);
 
 module.exports = router;
