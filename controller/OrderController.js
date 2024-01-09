227c83df87bc9201e444bdb7ca20c07d1270624e
sungohki <sokim1201@naver.com>
2024-01-09 14:23:17 +0900

    feat: Create a basic function

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,7 +1,31 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
-const ex = (req, res) => {
+const order = (req, res) => {
+  const sql = ``;
+  const values = [];
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    res.status(StatusCodes.OK).json(results);
+  });
+};
+const getOrders = (req, res) => {
+  const sql = ``;
+  const values = [];
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    res.status(StatusCodes.OK).json(results);
+  });
+};
+const getOrderDetail = (req, res) => {
   const sql = ``;
   const values = [];
 
@@ -14,4 +38,4 @@ const ex = (req, res) => {
   });
 };
 
-module.exports = {};
+module.exports = { order, getOrders, getOrderDetail };
