b5321be58b67cc95ed4d77cec45b6ceeb22fb16f
sungohki <sokim1201@naver.com>
2024-01-09 17:48:56 +0900

    feat: Testing the "Order” feature

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -2,15 +2,55 @@ const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
 const order = (req, res) => {
-  const sql = ``;
-  const values = [];
+  const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } =
+    req.body;
+  let sql = `
+  INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);
+  `;
+  let values = [delivery.address, delivery.receiver, delivery.contact];
+  let delivery_id = 3;
+  let order_id = 2;
+  //   let delivery_id;
+  //   let order_id;
 
-  conn.query(sql, values, (err, results) => {
+  //   conn.query(sql, values, (err, results) => {
+  //     if (err) {
+  //       console.log(err);
+  //       return res.status(StatusCodes.BAD_REQUEST).end();
+  //     }
+  //     delivery_id = results.insertId;
+
+  //     return res.status(StatusCodes.OK).json(results);
+  //   });
+  sql = `
+    INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+    VALUES (?, ?, ?, ?, ?);
+  `;
+  values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+  //   conn.query(sql, values, (err, results) => {
+  //     if (err) {
+  //       console.log(err);
+  //       return res.status(StatusCodes.BAD_REQUEST).end();
+  //     }
+  //     order_id = results.insertId;
+  //     console.log(order_id);
+
+  //     return res.status(StatusCodes.OK).json(results);
+  //   });
+  sql = `
+    INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;
+  `;
+  values = [];
+  items.forEach((item) => {
+    values.push([order_id, item.book_id, item.quantity]);
+  });
+  conn.query(sql, [values], (err, results) => {
     if (err) {
       console.log(err);
-      res.status(StatusCodes.BAD_REQUEST).end();
+      return res.status(StatusCodes.BAD_REQUEST).end();
     }
-    res.status(StatusCodes.OK).json(results);
+
+    return res.status(StatusCodes.OK).json(results);
   });
 };
 const getOrders = (req, res) => {
@@ -20,9 +60,9 @@ const getOrders = (req, res) => {
   conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
-      res.status(StatusCodes.BAD_REQUEST).end();
+      return res.status(StatusCodes.BAD_REQUEST).end();
     }
-    res.status(StatusCodes.OK).json(results);
+    return res.status(StatusCodes.OK).json(results);
   });
 };
 const getOrderDetail = (req, res) => {
@@ -32,9 +72,9 @@ const getOrderDetail = (req, res) => {
   conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
-      res.status(StatusCodes.BAD_REQUEST).end();
+      return res.status(StatusCodes.BAD_REQUEST).end();
     }
-    res.status(StatusCodes.OK).json(results);
+    return res.status(StatusCodes.OK).json(results);
   });
 };
 
