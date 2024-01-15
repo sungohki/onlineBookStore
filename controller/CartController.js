6c6c3dbe3173e64e877026392e839f0ed98b1317
sungohki <sokim1201@naver.com>
2024-01-15 17:13:59 +0900

    feat: Modify "user_id" with jwt token

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,14 +1,25 @@
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
+dotenv.config();
+
+// jwt 토큰 권한 체크
+const ensureAuthorization = (req) => {
+  const receivedJwt = req.headers['authorization'];
+  const decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+  return decodedJwt;
+};
 
 // 장바구니 아이템 추가
 const addToCart = (req, res) => {
-  const { book_id, quantity, user_id } = req.body;
+  const { book_id, quantity } = req.body;
+  const authorization = ensureAuthorization(req);
 
   const sql = `INSERT INTO 
     cartItems (book_id, quantity, user_id)
     VALUES (?, ?, ?);`;
-  const values = [book_id, quantity, user_id];
+  const values = [book_id, quantity, authorization.id];
   conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
@@ -20,13 +31,14 @@ const addToCart = (req, res) => {
 // 장바구니 아이템 목록 조회
 // + (장바구니에서 선택한) 주문 예상 상품 목록 조회
 const getCartItems = (req, res) => {
-  const { user_id, selected } = req.body;
+  const { selected } = req.body;
+  const authorization = ensureAuthorization(req);
 
   let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price
     FROM cartItems LEFT
     JOIN books ON cartItems.book_id = books.id
     WHERE user_id = ? AND cartItems.id IN (?);`;
-  const values = [user_id, selected];
+  const values = [authorization.id, selected];
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -38,11 +50,10 @@ const getCartItems = (req, res) => {
 };
 // 장바구니 아이템 삭제
 const removeCartItem = (req, res) => {
-  const { id } = req.params;
-  const { user_id } = req.body;
+  const cartItemId = req.params.id;
 
   const sql = `DELETE FROM cartItems WHERE id = ?`;
-  const values = [id, user_id];
+  const values = [cartItemId];
   conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
